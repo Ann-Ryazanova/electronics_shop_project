434eecd0dad4bb726449327054800442abf3c5b1
Ann-Ryazanova <zai97@mail.ru>
2023-04-30 11:39:02 +0300

    Added method __add__

--- src/item.py
+++ src/item.py
@@ -71,3 +71,9 @@ class Item:
 
     def __str__(self):
         return self.__name
+
+    def __add__(self, other):
+        if isinstance(other, self.__class__):
+            return self.quantity + other.quantity
+        else:
+            raise Exception
